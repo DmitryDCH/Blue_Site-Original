d41944778d6cc15e6017dd6ef270a182d0be3877
Dima Chekurkov <Chekurkov@gmail.com>
2019-02-10 20:39:11 +0200

    quick start

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, 'left');
+});
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, 'left');
+  });
